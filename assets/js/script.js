2198060c89570fcbe435cfb8da890da31b836d52
Samuelikz <samuel.silva@a.ficr.edu.br>
2020-09-15 22:01:09 -0300

    Vuejs/javascript

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+
+var app = new Vue({
+  el: '.texto',
+  data: {
+    samuel: 'Estudante do Curso de Análise e desenvolvimento de Sistemas pela Faculdade Católica Imaculada Conceição do Recife, apaixonado pela tecnologia e programação aprofundar meus conhecimentos na área de tecnologia é o meu maior foco, dominar as principais tecnologias utilizadas no mercado é uma meta.'
+  }
+})
+
+
