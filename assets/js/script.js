be92812481eea42469c54dc024909a9d00d3ce2d
Samuelikz <62406705+samuelikz@users.noreply.github.com>
2022-09-27 08:40:13 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var app = new Vue({
   el: '.texto',
   data: {
-    text: 'Analista e desenvolvimento de Sistemas formado pela Faculdade Católica Imaculada Conceição do Recife, apaixonado pela tecnologia e programação, Hello World!'
+    text: 'Analista e desenvolvedor de Sistemas formado pela Faculdade Católica Imaculada Conceição do Recife, apaixonado pela tecnologia e programação, Hello World!'
   }
 })
 
